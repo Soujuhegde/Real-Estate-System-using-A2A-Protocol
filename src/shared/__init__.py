8cb6591c43858f2cf1f0ab1c9312146e1b0d2ed7
Soujuhegde <soujanyasp27@gmail.com>
2026-06-05 16:36:53 +0530

    Initialize shared package

---
+++ src/shared/__init__.py
@@ -0,0 +1,6 @@
+from .a2a_models import (
+    A2AMessage, A2ATaskRequest, A2ATaskResponse,
+    A2ATaskStatus, A2AArtifact, AgentCard, AgentSkill, A2APart
+)
+from .a2a_client import A2AClient
+from . import config
